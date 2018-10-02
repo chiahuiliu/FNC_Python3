235b3a0e9f0209a8df5aba2fad0ed8a3aeefc08d
ChiaHui Liu <chiahuiliu@utexas.edu>
2018-10-02 18:31:14 -0500

    update FeatureGenerator.py

--- FeatureGenerator.py
+++ FeatureGenerator.py
@@ -1,37 +1,121 @@
-'''
-    super class of various feature generators
-'''
+import ngram
+import pandas as pd
+import numpy as np
+import cPickle
+from helpers import *
+from sklearn.model_selection import train_test_split
+from CountFeatureGenerator import *
+from TfidfFeatureGenerator import *
+from SvdFeatureGenerator import *
+from Word2VecFeatureGenerator import *
+from SentimentFeatureGenerator import *
+#from AlignmentFeatureGenerator import *
 
-class FeatureGenerator(object):
+def process():
 
-    def __init__(self, name):
-        self._name = name
-    
-    def name(self):
-        return self._name
+    full_data = pd.read_csv('./data/merged_data_tain.csv', encoding='utf-8')
+    used_column = ['claimHeadline', 'articleHeadline', 'claimTruthiness', 'articleStance']
+    full_data = full_data[used_column]
+    train, test = train_test_split(full_data, test_size=0.33, random_state=1234)
 
-    def process(self, data, header):
-        '''
-            input:
-                data: pandas dataframe
-            generate features and save them into a pickle file
-        '''
-        pass
+    read = False
+    if not read:
 
-    def read(self, header):
-        '''
-            read the feature matrix from a pickle file
-        '''
-        pass
+        targets = ['observing', 'for', 'against', 'ignoring']
+        targets_dict = dict(zip(targets, range(len(targets))))
+        train['target'] = map(lambda x: targets_dict[x], train['articleStance'])
+        print 'train.shape:'
+        print train.shape
+        n_train = train.shape[0]
+
+        data = train
+        # read test set, no 'Stance' column in test set -> target = NULL
+        # concatenate training and test set
+        test_flag = True
+        if test_flag:
+
+            data = full_data
+            print data
+            print 'data.shape:'
+            print data.shape
+
+            train = data[~data['target'].isnull()]
+            print train
+            print 'train.shape:'
+            print train.shape
+
+            test = data[data['target'].isnull()]
+            print test
+            print 'test.shape:'
+            print test.shape
+
+        #data = data.iloc[:100, :]
+
+        #return 1
+
+        print "generate unigram"
+        data["claimHeadline_unigram"] = data["claimHeadline"].map(lambda x: preprocess_data(x))
+        data["articleHeadline_unigram"] = data["articleHeadline"].map(lambda x: preprocess_data(x))
+
+        print "generate bigram"
+        join_str = "_"
+        data["claimHeadline_bigram"] = data["claimHeadline_unigram"].map(lambda x: ngram.getBigram(x, join_str))
+        data["articleHeadline_bigram"] = data["articleHeadline_unigram"].map(lambda x: ngram.getBigram(x, join_str))
+
+        print "generate trigram"
+        join_str = "_"
+        data["claimHeadline_trigram"] = data["claimHeadline_unigram"].map(lambda x: ngram.getTrigram(x, join_str))
+        data["articleHeadline_trigram"] = data["articleHeadline_bigram"].map(lambda x: ngram.getTrigram(x, join_str))
+
+        with open('data.pkl', 'wb') as outfile:
+            cPickle.dump(data, outfile, -1)
+            print 'dataframe saved in data.pkl'
+
+    else:
+        with open('data.pkl', 'rb') as infile:
+            data = cPickle.load(infile)
+            print 'data loaded'
+            print 'data.shape:'
+            print data.shape
+    #return 1
+
+    # define feature generators
+    countFG    = CountFeatureGenerator()
+    tfidfFG    = TfidfFeatureGenerator()
+    svdFG      = SvdFeatureGenerator()
+    word2vecFG = Word2VecFeatureGenerator()
+    sentiFG    = SentimentFeatureGenerator()
+    #walignFG   = AlignmentFeatureGenerator()
+    generators = [countFG, tfidfFG, svdFG, word2vecFG, sentiFG]
+    #generators = [svdFG, word2vecFG, sentiFG]
+    #generators = [tfidfFG]
+    #generators = [countFG]
+    #generators = [walignFG]
+
+    for g in generators:
+        g.process(data)
+
+    for g in generators:
+        g.read('train')
+
+    #for g in generators:
+    #    g.read('test')
+
+    print 'done'
+
+
+if __name__ == "__main__":
+
+    process()
 
  #   Copyright 2017 Cisco Systems, Inc.
- #  
+ #
  #   Licensed under the Apache License, Version 2.0 (the "License");
  #   you may not use this file except in compliance with the License.
  #   You may obtain a copy of the License at
- #  
+ #
  #     http://www.apache.org/licenses/LICENSE-2.0
- #  
+ #
  #   Unless required by applicable law or agreed to in writing, software
  #   distributed under the License is distributed on an "AS IS" BASIS,
  #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
