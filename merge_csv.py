25fe011d634b9824f7eb583406d3afac3913d5e8
ChiaHui Liu <chiahuiliu@utexas.edu>
2018-10-01 19:40:58 -0500

    final updates

--- merge_csv.py
+++ merge_csv.py
@@ -21,7 +21,18 @@ def merge_fnc_article_body(article_df, body_df):
     merged_df = article_df.merge(body_df, left_on='Body ID', right_on='Body ID', how='left')
     return merged_df
 
+
+def merge_w_emergent(df_fnc):
+    df_emergent = pd.read_csv('data/url-versions-2015-06-14.csv')
+    df_merged_res = df_fnc.merge(df_emergent, left_on='Headline', right_on='articleHeadline', how='inner')
+    df_merged_res.to_csv('./data/merged_w_allCols.csv', index=False)
+    final_cols = ['claimId', 'claimHeadline', 'articleId','articleHeadline', 'claimTruthiness', 'articleStance']
+    df_final_res = df_merged_res[final_cols]
+    df_final_res = df_final_res.drop_duplicates()
+    df_final_res.to_csv('./data/merged_data_tain.csv', index=False)
+
 df_fnc_stance,  df_fnc_bodies= read_csvs()
 df_fnc = merge_fnc_article_body(df_fnc_stance, df_fnc_bodies)
 
 df_fnc.to_csv('./data/fnc_data.csv', index=False)
+merge_w_emergent(df_fnc)
